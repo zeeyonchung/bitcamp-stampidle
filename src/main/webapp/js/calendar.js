95b7d5ea5fe5d43d2117ce8b8d02495284df879d
zeeyon chung <zeeyon.c@gmail.com>
2017-02-13 06:59:34 +0900

    .

--- src/main/webapp/js/calendar.js
+++ src/main/webapp/js/calendar.js
@@ -54,7 +54,8 @@ $(function()
 
 	$('#date-custom').dateRangePicker(
 			{
-				separator : ' ~ '
+				separator : ' ~ ',
+				autoClose: true
 
 			}).bind('datepicker-first-date-selected', function(event, obj)
 					{
@@ -103,118 +104,6 @@ $(function()
 																	});
 
 
-
-
-
-
-
-
-	$('#date-day').dateRangePicker(
-			{
-				autoClose: true,
-				singleDate : true,
-				showShortcuts: false,
-				endDate: '2017-02-10'
-			}).bind('datepicker-first-date-selected', function(event, obj)
-					{
-				/* This event will be triggered when first date is selected */
-				////console.log('first-date-selected',obj);
-				// obj will be something like this:
-				// {
-				//    date1: (Date object of the earlier date)
-				// }
-					})
-					.bind('datepicker-change',function(event,obj)
-							{
-						/* This event will be triggered when second date is selected */
-						////console.log('change',obj);
-						// obj will be something like this:
-						// {
-						// 		date1: (Date object of the earlier date),
-						// 		date2: (Date object of the later date),
-						//	 	value: "2013-06-05 to 2013-06-07"
-						// }
-							})
-							.bind('datepicker-apply',function(event,obj)
-									{
-								/* This event will be triggered when user clicks on the apply button */
-								//console.log('apply',obj);
-									})
-									.bind('datepicker-close',function()
-											{
-										/* This event will be triggered before date range picker close animation */
-										//console.log('before close');
-											})
-											.bind('datepicker-closed',function()
-													{
-												/* This event will be triggered after date range picker close animation */
-												//console.log('after close');
-													})
-													.bind('datepicker-open',function()
-															{
-														/* This event will be triggered before date range picker open animation */
-														//console.log('before open');
-															})
-															.bind('datepicker-opened',function()
-																	{
-																/* This event will be triggered after date range picker open animation */
-																//console.log('after open');
-																	});
-
-
-
-	$('#date-week').dateRangePicker(
-			{
-				autoClose: true,
-				batchMode: 'week',
-				singleMonth: true,
-				separator : ' ~ '
-
-			}).bind('datepicker-first-date-selected', function(event, obj)
-					{
-				/* This event will be triggered when first date is selected */
-				//console.log('first-date-selected',obj);
-				// obj will be something like this:
-				// {
-				//    date1: (Date object of the earlier date)
-				// }
-					})
-					.bind('datepicker-change',function(event,obj)
-							{
-						/* This event will be triggered when second date is selected */
-						//console.log('change',obj);
-						// obj will be something like this:
-						// {
-						// 		date1: (Date object of the earlier date),
-						// 		date2: (Date object of the later date),
-						//	 	value: "2013-06-05 to 2013-06-07"
-						// }
-							})
-							.bind('datepicker-apply',function(event,obj)
-									{
-								/* This event will be triggered when user clicks on the apply button */
-								//console.log('apply',obj);
-									})
-									.bind('datepicker-close',function()
-											{
-										/* This event will be triggered before date range picker close animation */
-										//console.log('before close');
-											})
-											.bind('datepicker-closed',function()
-													{
-												/* This event will be triggered after date range picker close animation */
-												//console.log('after close');
-													})
-													.bind('datepicker-open',function()
-															{
-														/* This event will be triggered before date range picker open animation */
-														//console.log('before open');
-															})
-															.bind('datepicker-opened',function()
-																	{
-																/* This event will be triggered after date range picker open animation */
-																//console.log('after open');
-																	});
 
 
 	$('.single-month .month-wrapper').css('margin-left', '43px');
