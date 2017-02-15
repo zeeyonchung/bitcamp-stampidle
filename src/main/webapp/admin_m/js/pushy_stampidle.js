df0386346ed58fb2641e7f628f7e88df6947bdcb
sinaeYu <anne.sinae@gmail.com>
2017-02-15 10:06:05 +0900

    ,

---
+++ src/main/webapp/admin_m/js/pushy_stampidle.js
@@ -0,0 +1,9 @@
+var optionSideLeft = {
+            button: "side-left-pushy-button",
+            container: "container",
+            containerPush: false,
+            menuPosition: "left",
+            menuOpen: false,
+            overlayShow: true          
+        }
+        $("#sideLeftPushy").Pushy(optionSideLeft);
