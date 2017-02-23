46a6e21a90cc1ee06013794e3662b1358d672ba4
Zeeyon Chung <zeeyon.c@gmail.com>
2017-02-23 17:15:33 +0900

    .

--- src/main/java/bitcamp/java89/ems2/domain/CafeMember.java
+++ src/main/java/bitcamp/java89/ems2/domain/CafeMember.java
@@ -6,24 +6,22 @@ public class CafeMember implements Serializable {
   
   private static final long serialVersionUID = 1L;
   protected int cafeMemberNo;
-  protected String iD;
+  protected String id;
   protected String cellNo;
   protected String companyNo;
   protected String Email;
   protected String password;
-  
-  
   public int getCafeMemberNo() {
     return cafeMemberNo;
   }
   public void setCafeMemberNo(int cafeMemberNo) {
     this.cafeMemberNo = cafeMemberNo;
   }
-  public String getiD() {
-    return iD;
+  public String getId() {
+    return id;
   }
-  public void setiD(String iD) {
-    this.iD = iD;
+  public void setId(String id) {
+    this.id = id;
   }
   public String getCellNo() {
     return cellNo;
@@ -49,6 +47,7 @@ public class CafeMember implements Serializable {
   public void setPassword(String password) {
     this.password = password;
   }
+  
   
   
 }
