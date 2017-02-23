aaaa1fd52d11e202e279460df72b585c43d123d7
GraysonSunkyoJung <graysons.jung@gmail.com>
2017-02-23 11:14:14 +0900

    .

---
+++ src/main/webapp/cstmr/js/link.js
@@ -0,0 +1,15 @@
+/*****  sidebar.html  *****/
+
+
+// 지나간 이벤트 클릭.
+$('.pastevent').click(function(e) {
+	location.href = 'pastevent.html'
+});
+
+//진행중인 이벤트 클릭(지나간 이벤트 에서)
+$('.now-event').click(function(e) {
+	location.href = 'event.html'
+});
+
+/*****  main/main.html  *****/
+
