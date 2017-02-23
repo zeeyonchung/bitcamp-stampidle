a7639708d5afbe436702480d008c085d992f58a0
GraysonSunkyoJung <graysons.jung@gmail.com>
2017-02-23 21:04:03 +0900

    로그인

--- src/main/java/bitcamp/java89/ems2/control/AuthControl.java
+++ src/main/java/bitcamp/java89/ems2/control/AuthControl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import bitcamp.java89.ems2.domain.Member;
+import bitcamp.java89.ems2.domain.CafeMember;
 import bitcamp.java89.ems2.service.AuthService;
 
 @Controller
@@ -17,46 +17,46 @@ public class AuthControl {
   
   @Autowired AuthService authService;
   
-  @RequestMapping("/auth/login")
-  public String login(String email, String password, boolean saveEmail, String userType,
+  @RequestMapping("/admin/auth/login")
+  public String login(String id, String password, boolean saveId,
       HttpServletResponse response, HttpSession session, Model model) throws Exception {
-    if (saveEmail) {
+    if (saveId) {
       // 쿠키를 웹 브라우저에게 보낸다.
-      Cookie cookie = new Cookie("email", email);
+      Cookie cookie = new Cookie("id", id);
       cookie.setMaxAge(60 * 60 * 24 * 15);
       response.addCookie(cookie);
       
     } else {
       // 기존에 보낸 쿠키를 제거하라고 웹 브라우저에게 응답한다.
-      Cookie cookie = new Cookie("email", "");
+      Cookie cookie = new Cookie("id", "");
       cookie.setMaxAge(0);
       response.addCookie(cookie);
     }
     
-    Member member = authService.getMemberInfo(email, password, userType);
+    CafeMember cafeMember = authService.getCafeMemberInfo(id, password);
         
-    if (member == null) {
+    if (cafeMember == null) {
       response.setHeader("Refresh", "2;url=loginform.do");
       model.addAttribute("title", "로그인");
       model.addAttribute("contentPage", "auth/loginfail.jsp");
       return "main";
     }
     
-    session.setAttribute("member", member); // HttpSession에 저장한다.
-    return "redirect:../student/list.do";
+    session.setAttribute("id", id); // HttpSession에 저장한다.
+    return "redirect:../main/main";
   }
   
-  @RequestMapping("/auth/loginform")
+  @RequestMapping("/admin/auth/loginform")
   public String loginform(Model model) throws Exception {
     model.addAttribute("title", "로그인");
     model.addAttribute("contentPage", "auth/loginform.jsp");
     return "main";
   }
   
-  @RequestMapping("/auth/logout")
+  @RequestMapping("/admin/auth/logout")
   public String logout(HttpSession session) throws Exception {
     session.invalidate(); // 기존 세션을 무효화시킨다.
-    return "redirect:loginform.do";
+    return "redirect:";
   }
 }
 
--- src/main/java/bitcamp/java89/ems2/service/impl/AuthServiceImpl.java
+++ src/main/java/bitcamp/java89/ems2/service/impl/AuthServiceImpl.java
@@ -5,40 +5,26 @@ import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import bitcamp.java89.ems2.dao.ManagerDao;
-import bitcamp.java89.ems2.dao.MemberDao;
-import bitcamp.java89.ems2.dao.StudentDao;
-import bitcamp.java89.ems2.dao.TeacherDao;
-import bitcamp.java89.ems2.domain.Member;
+import bitcamp.java89.ems2.dao.CafeMemberDao;
+import bitcamp.java89.ems2.domain.CafeMember;
 import bitcamp.java89.ems2.service.AuthService;
 
 @Service
 public class AuthServiceImpl implements AuthService {
   
-  @Autowired MemberDao memberDao;
-  @Autowired StudentDao studentDao;
-  @Autowired ManagerDao managerDao;
-  @Autowired TeacherDao teacherDao;
+  @Autowired CafeMemberDao cafeMemberDao;
   
-  public Member getMemberInfo(String email, String password, String userType) throws Exception {
+  public CafeMember getCafeMemberInfo(String id, String password) throws Exception {
     HashMap<String,String> paramMap = new HashMap<>();
-    paramMap.put("email", email);
+    paramMap.put("id", id);
     paramMap.put("password", password);
     
-    Member member = memberDao.getOneByEmailPassword(paramMap);
-    
-    if (member == null) {
+    System.out.println(paramMap);
+    CafeMember cafeMember = cafeMemberDao.getOneByIdPassword(paramMap);
+    System.out.println(cafeMember);
+    if (cafeMember == null) {
       return null;
     }
-    
-    if (userType.equals(Member.STUDENT)) {
-      return studentDao.getOne(member.getMemberNo());
-      
-    } else if (userType.equals(Member.TEACHER)) {
-      return teacherDao.getOneWithPhoto(member.getMemberNo());
-      
-    } else /*if (userType.equals(Member.MANAGER))*/ {
-      return managerDao.getOne(member.getMemberNo());
-    }
+    return cafeMember;
   }
 }
