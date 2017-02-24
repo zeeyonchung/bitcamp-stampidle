966128aa8e24eb6dfcf7fea4ff799701c3b3581b
jeonghun Ji <jj.jeonghun.ji@gmail.com>
2017-02-24 11:59:06 +0900

    .

--- src/main/webapp/admin/event/eventadd.js
+++ src/main/webapp/admin/event/eventadd.js
@@ -1,15 +1,23 @@
+var now = new Date();
+      var year= now.getFullYear();
+      var mon = (now.getMonth()+1)>9 ? ''+(now.getMonth()+1) : '0'+(now.getMonth()+1);
+      var day = now.getDate()>9 ? ''+now.getDate() : '0'+now.getDate();
+              
+      var chan_val = year + '-' + mon + '-' + day;
 
+
+출처: http://wfreud.tistory.com/21 [wfreud 개인 라이브러리]
 
 $('.event-regi-btn').click(function() {
     var param = {
 		"cafeNo": 10,
 		"eventTitle": $('.event-title').val(),
 		"eventContents": $('.event-contents').val(),
-		"registDate": '2012-12-12',
+		"registDate": chan_val,
 		"eventPhotoPath": '../image/cafe_event.jpg',
 		"eventView": 0,
-		"startDate": '2017-02-11',
-		"endDate": '2017-02-11', 
+		"startDate": $('.startDate').val(),
+		"endDate": $('.endDate').val() 
     };
     console.log(param);
     
