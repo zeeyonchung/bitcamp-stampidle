df223df13ef6873720fe2f511395584de0fbf353
sinaeYu <anne.sinae@gmail.com>
2017-02-27 04:59:32 +0900

    topSearch json convert to javascript array test

--- src/main/webapp/admin/js/auto_complete/srchCstmr.js
+++ src/main/webapp/admin/js/auto_complete/srchCstmr.js
@@ -1,28 +1,36 @@
 $.getJSON(serverRoot + '/customMember/srchList.json', function(ajaxResult) {
-  var status = ajaxResult.status;
-  
-  if (status != "success")
-	  return;
-  
-  var srchList = ajaxResult.data;
+	var status = ajaxResult.status;
 
-var availableTags = [
-                        '가나',
-                        '가나쵸콜렛',
-                        '가오리',
-                        '가상현실',
-                        '북마크',
-                        '북까페',
-                        '북촌한옥마을',
-                        '010-3238-0101',
-                        '010-3338-0222'
-                    ];
+	if (status != "success")
+		return;
 
+	var jsonSrchList = ajaxResult.data;
+
+	var arrSrchList = $.map(jsonSrchList, function(jdata) { return jdata }); 
+
+	
+	$.each(arrSrchList, function (index, value) {
+        console.log(value);
+    });
+	
+	var test = "test";
+	var availableTags = [
+        '가나' + test,
+        '가나쵸콜렛',
+        '가오리',
+        '가상현실',
+        '북마크',
+        '북까페',
+        '북촌한옥마을',
+        '010-3238-0101',
+        '010-3338-0222'
+    ];
+	    
 	$(document).ready(function() {
-	    $("#searchbox").autocomplete(availableTags,{ 
-	        matchContains: true,
-	        selectFirst: false
-	    });
+		$("#searchbox").autocomplete(availableTags, {
+			matchContains : true,
+			selectFirst : false
+		});
 	});
 });
 
