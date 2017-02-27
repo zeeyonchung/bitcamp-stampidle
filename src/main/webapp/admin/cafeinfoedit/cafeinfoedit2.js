8be144342d19dce9c80bd2219e9d855de74dad71
jeonghun Ji <jj.jeonghun.ji@gmail.com>
2017-02-27 10:06:36 +0900

    .

--- src/main/webapp/admin/cafeinfoedit/cafeinfoedit2.js
+++ src/main/webapp/admin/cafeinfoedit/cafeinfoedit2.js
@@ -11,7 +11,6 @@ $('#photo').fileupload({
         .test(window.navigator && navigator.userAgent), // 안드로이드와 오페라 브라우저는 크기 조정 비활성 시키기
     previewMaxWidth: 800,   // 미리보기 이미지 너비
     previewMaxHeight: 800,  // 미리보기 이미지 높이 
-    previewCrop: true,      // 미리보기 이미지를 출력할 때 원본에서 지정된 크기로 자르기
     done: function (e, data) { // 서버에서 응답이 오면 호출된다. 각 파일 별로 호출된다.
     	console.log('done()...');
     	console.log(data.result);
@@ -20,7 +19,6 @@ $('#photo').fileupload({
     processalways: function(e, data) {
         console.log('fileuploadprocessalways()...', data.files.length, data.index);
         var img = $('#photo-img');
-        var img2 = &('#photo-img2')
         if (data.index == 0) {
         	console.log('미리보기 처리...');
 	        var canvas = data.files[0].preview;
@@ -28,14 +26,6 @@ $('#photo').fileupload({
 	        img.attr('src', dataURL).css('width', '100%');
 	        img.attr('src', dataURL).css('height', '100%');
 	        $('#photo-label').css('display', '');
-        } else (data.index == 1) {
-        	console.log('미리보기 처리...');
-	        var canvas = data.files[1].preview;
-	        var dataURL = canvas.toDataURL();
-	        img2.attr('src', dataURL).css('width', '100%');
-	        img2.attr('src', dataURL).css('height', '100%');
-	        $('#photo-label').css('display', '');
-        	
-        }
+        } 
     } 
 });
