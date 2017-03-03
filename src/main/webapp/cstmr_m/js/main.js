d1e468d844162f763feb1fe241f20c963129741a
GraysonSunkyoJung <graysons.jung@gmail.com>
2017-03-03 12:30:34 +0900

    고객 모바일 로그인,아웃 메인이름적용

---
+++ src/main/webapp/cstmr_m/js/main.js
@@ -0,0 +1,14 @@
+/*로그인 정보를 가져와서*/
+$.getJSON(serverRoot + '/auth/loginUser.json', function(ajaxResult) {
+	if (ajaxResult.status != "success") {
+		console.log(ajaxResult.data);
+		location.href = clientRoot + "/login.html";
+		
+	}
+	var userData = ajaxResult.data;
+	var userName = userData.name;
+	
+	console.log(userName);
+	$('h2 > span').append(userName);
+	
+});
