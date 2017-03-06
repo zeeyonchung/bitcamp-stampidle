f72a37583cbcfe762a4801dfe3dfbc0e28aad590
sinaeYu <anne.sinae@gmail.com>
2017-03-06 15:30:52 +0900

    dao/js edit to cmno, multi image upload for cafeinfoedit

--- src/main/java/bitcamp/java89/ems2/service/CafeTimeService.java
+++ src/main/java/bitcamp/java89/ems2/service/CafeTimeService.java
@@ -4,7 +4,7 @@ import bitcamp.java89.ems2.domain.CafeTime;
 
 public interface CafeTimeService {
   int add(CafeTime cafeTime) throws Exception;
-  CafeTime detailTime(int cafeNo) throws Exception;
+  CafeTime detailTime(int cafeMemberNo) throws Exception;
 }
 
 
