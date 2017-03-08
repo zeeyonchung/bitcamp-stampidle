db6645345a6d36e414bc694b8ac4f7940417d890
Zeeyon Chung <zeeyon.c@gmail.com>
2017-03-08 20:08:48 +0900

    frontcard file upload

--- src/main/webapp/admin/cardadd/cardadd.js
+++ src/main/webapp/admin/cardadd/cardadd.js
@@ -161,7 +161,7 @@ $('.btmsubmit').click (function() {
 	var paramCard = {
 			"cafeMemberNo": cafeMemberNo,
 			"stampCount": $('.midNum').text(),
-			"frontImgPath": $('.frontcard').attr("src"),
+			"frontImgPath": $('#front-photo-path').val(),
 			"backImgPath": $('#back-photo-path').val(),
 			"stampImgPath": $('#photo-path').val(),
 			"service": $('.service').text()
@@ -258,6 +258,34 @@ $('#back-fileupload').fileupload({
 	        updateStmpsideSize();
 	        updateMidtopSize();
 	        updateBtmlineSize();
+        }
+    } 
+});
+
+
+
+/************ 앞면 이미지 저장 **************/
+$('#front-fileupload').fileupload({
+    url: serverRoot + '/../common/fileupload.json', // 서버에 요청할 URL
+    dataType: 'json',         // 서버가 보낸 응답이 JSON임을 지정하기
+    sequentialUploads: true,  // 여러 개의 파일을 업로드 할 때 순서대로 요청하기.
+    singleFileUploads: false, // 한 요청에 여러 개의 파일을 전송시키기. 기본은 true.
+    autoUpload: true,        // 파일을 추가할 때 자동 업로딩 여부 설정. 기본은 true.
+    disableImageResize: /Android(?!.*Chrome)|Opera/
+        .test(window.navigator && navigator.userAgent), // 안드로이드와 오페라 브라우저는 크기 조정 비활성 시키기
+    done: function (e, data) { // 서버에서 응답이 오면 호출된다. 각 파일 별로 호출된다.
+    	console.log('done()...');
+    	console.log(data.result);
+        $('#front-photo-path').val(data.result.data[0]);
+    }, 
+    processalways: function(e, data) {
+        console.log('fileuploadprocessalways()...', data.files.length, data.index);
+        var img = $('#front-photo-img');
+        if (data.index == 0) {
+        	console.log('미리보기 처리...');
+	        var canvas = data.files[0].preview;
+	        var dataURL = canvas.toDataURL();
+	        img.attr('src', dataURL).css('width', '130px');
         }
     } 
 });
