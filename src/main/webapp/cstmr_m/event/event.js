4c20ee42f6319b85f078b941966a5c80a7f211f9
Zeeyon Chung <zeeyon.c@gmail.com>
2017-03-13 16:48:56 +0900

    .

---
+++ src/main/webapp/cstmr_m/event/event.js
@@ -0,0 +1,56 @@
+$.getJSON(serverRoot + '/auth/loginUser.json', function(ajaxResult) {
+	if (ajaxResult.status != "success") {
+		console.log(ajaxResult.data);
+		location.href = clientRoot + "/auth/login.html";
+		/*로그인 안 했으면 로그인 페이지로 보내기*/
+	}
+	var member = ajaxResult.data;
+	var memberNo = member.memberNo;
+	var postNo=10;
+    var i = 1;
+    loadPage(i)
+    
+	$(window).scroll(function(){
+		{
+	      if($(window).scrollTop()+100 >= $(document).height() - $(window).height())
+	    	  {
+	    	  loadPage(++i);
+	    	  }
+	    		  
+	    }
+	});
+	
+
+	function loadPage(pageCount) {
+		console.log(pageCount)
+	$.getJSON(
+	serverRoot + '/event/getAllList.json',
+	{
+	 'pageCount': pageCount,
+	 'postNo': postNo,
+	 }, 
+	function(ajaxResult) {
+	  var status = ajaxResult.status;
+	  if (status != "success") {console.log(ajaxResult.data); return;}
+	  var list = ajaxResult.data.list;
+
+	  var eventdiv = $('#container');
+	  var template = Handlebars.compile($('#trTemplate').html());
+	  $('#container').append(template({"list": list}));
+	  
+	  $('.title').click(function(event) {
+		event.preventDefault();
+		$.getJSON(serverRoot + '/event/updateView.json?eventNo=' + $(this).attr("data-no"));
+		
+	  	location.href = 'eventdetail.html?eventNo=' + $(this).attr("data-no");
+	  });
+	  
+	  
+	});
+	};
+	
+	
+});
+$('#top-btn').click(function(event) {
+	$(window).scrollTop($(window).height);
+});
