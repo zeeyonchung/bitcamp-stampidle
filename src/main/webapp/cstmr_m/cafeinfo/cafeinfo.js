939224e79e7fb2ade22e317b17294ace6fb846c5
GraysonSunkyoJung <graysons.jung@gmail.com>
2017-03-14 16:38:13 +0900

    cstmr_m cafeinfo 슬라이드1

--- src/main/webapp/cstmr_m/cafeinfo/cafeinfo.js
+++ src/main/webapp/cstmr_m/cafeinfo/cafeinfo.js
@@ -1,19 +1,35 @@
+$.getJSON(serverRoot + '/auth/loginUser.json', function(ajaxResult) {
+	if (ajaxResult.status != "success") {
+		console.log(ajaxResult.data);
+		location.href = clientRoot + "/auth/login.html";
+		/*로그인 안 했으면 로그인 페이지로 보내기*/
+	}
+	var userNo = ajaxResult.data.customMemberNo;
 	var cafeMembNo = 1
-	
+
 	$.getJSON(serverRoot + '/cafe/detail.json?cafeMemberNo=' + cafeMembNo, function(ajaxResult) {
 		var cafe = ajaxResult.data;
 		$('.cafe-name').text(cafe.cafeName);
 		$('.cafe-info').text(cafe.intro);
 		$('.cafe-addr').text(cafe.address +" "+ cafe.detailAddress);
 		$('.cafe-tel').text(cafe.cafeTel);
-		
-		$.getJSON(serverRoot + '/cardadd/getCafeCardDetail.json?cafeMemberNo=' + cafeMembNo, function(ajaxResult) {
-			var cardInfo = ajaxResult.data;
-			console.log(cardInfo);
-			$.each(cardInfo, function(i) {
-				$('.stmpcard').attr('src', '../'+cardInfo[i].backImgPath);
-				$('.stmpcard2').attr('src', '../'+cardInfo[i].frontImgPath);
+
+		$.getJSON(serverRoot + '/customCard/customDetail.json', 
+				{'customMemberNo': userNo,
+			'cafeMemberNo': cafeMembNo},
+			function(ajaxResult) {
+				var stmpNo = ajaxResult.data.allStampCount;
+
+				$.getJSON(serverRoot + '/cardadd/getCafeCardDetail.json?cafeMemberNo=' + cafeMembNo, function(ajaxResult) {
+					var cardInfo = ajaxResult.data;
+					$.each(cardInfo, function(i) {
+						$('.stmpcard').attr('src', '../'+cardInfo[i].backImgPath);
+						$('.stmpcard2').attr('src', '../'+cardInfo[i].frontImgPath);
+						var many = cardInfo[i].stampCount;
+						$('.stmp-circle').text(stmpNo + "/" + many);
+					});
+				});
 			});
-		});
+	});
 });
 
