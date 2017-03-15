d50880d31cbafbd9e8edfa9b62a15ba13e6c3190
Zeeyon Chung <zeeyon.c@gmail.com>
2017-03-15 17:53:08 +0900

    .

--- src/main/webapp/cstmr_m/js/mystamp.js
+++ src/main/webapp/cstmr_m/js/mystamp.js
@@ -1,3 +1,28 @@
+$(window).load(function(){
+    if (!document.getElementById('tab_swipe')) return false;
+    var mnTab = document.getElementById('tab_swipe').getElementsByTagName('li');
+    var elem = document.getElementById('swipe');
+    
+    window.areaSwipe = Swipe(elem, {
+        startSlide: 0,
+        continuous: true,
+        callback: function(pos) {
+            var currentSlide = areaSwipe.getPos() + 1;
+            var n = mnTab.length;
+            while (n--) {
+                mnTab[n].className = ' ';
+            }
+            mnTab[pos].className = 'on';
+            
+			var secH = $(".section:nth-of-type(" + currentSlide + ")").outerHeight();
+            $("#swipe .swWrap").css('height', secH);
+            $(window).scrollTop(0);
+            console.log(currentSlide, $(".section:nth-of-type(1)").outerHeight(), $(".section:nth-of-type(2)").outerHeight(), $(".section:nth-of-type(3)").outerHeight());
+        }
+    });
+});
+
+
 /*로그인 정보를 가져와서*/
 $.getJSON(serverRoot + '/auth/loginUser.json', function(ajaxResult) {
 	if (ajaxResult.status != "success") {
@@ -12,7 +37,6 @@ $.getJSON(serverRoot + '/auth/loginUser.json', function(ajaxResult) {
 	$.getJSON(serverRoot + '/customCard/getMyCardList.json?customMemberNo=' + userNo, function(ajaxResult) {
 		var myCardList = ajaxResult.data;
 		console.log(myCardList);
-		
 		if (myCardList == null) {
 			$('.section.all #num').text(0);
 		} else {
@@ -29,7 +53,6 @@ $.getJSON(serverRoot + '/auth/loginUser.json', function(ajaxResult) {
 	$.getJSON(serverRoot + '/customCard/getMyFavoriteCardList.json?customMemberNo=' + userNo, function(ajaxResult) {
 		var myFavoriteCardList = ajaxResult.data;
 		console.log(myFavoriteCardList);
-		
 		if (myFavoriteCardList == null) {
 			$('.section.favorite #num').text(0);
 		} else {
@@ -46,7 +69,6 @@ $.getJSON(serverRoot + '/auth/loginUser.json', function(ajaxResult) {
 	$.getJSON(serverRoot + '/customCard/getMyFinishCardList.json?customMemberNo=' + userNo, function(ajaxResult) {
 		var myFinishCardList = ajaxResult.data;
 		console.log(myFinishCardList);
-		
 		if (myFinishCardList == null) {
 			$('.section.done #num').text(0);
 		} else {
