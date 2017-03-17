6c96b82cfa086040a0231862a1ae82f88a2e09a2
Zeeyon Chung <zeeyon.c@gmail.com>
2017-03-17 13:01:31 +0900

    .

--- src/main/webapp/cstmr_m/js/findcafe.js
+++ src/main/webapp/cstmr_m/js/findcafe.js
@@ -30,51 +30,53 @@ var allCafeCount = 0;
 
 /**** 글 불러오기 ****/
 function loadPage(pageCount) {
+	console.log(customMemberNo, searchKeyword, postNo, pageCount);
 	// 나중에 파라미터로 나의 현재 위치도 넘겨야 함. 지금은 그냥 목록 다 꺼내오는 것.
 	$.getJSON(
-			serverRoot + '/customCard/findCafe.json',
-			{customMemberNo: customMemberNo,
-				searchKeyword: searchKeyword,
-				postNo: postNo,
-				pageCount: pageCount},
-				
-				function(ajaxResult) {
-					var status = ajaxResult.status;
-					if (status != "success") {console.log(ajaxResult.data); return;}
-					var cafeList = ajaxResult.data.cafeList;
-					console.log(cafeList);
-					
-					if (cafeList == null) {
-						$('.cafeListArea #num').text(allCafeCount);
-					} else {
-						allCafeCount = ajaxResult.data.allCafeCount;
-						$('.cafeListArea #num').text(allCafeCount);
-					}
-					
-					var listArea = $('.listArea');
-					var template = Handlebars.compile($('#trTemplate').html());
-					
-					if (pageCount == 1 && searchKeyword != "") {
-						listArea.html(template({"cafeList": cafeList}));
-					} else {
-						listArea.append(template({"cafeList": cafeList}));
-					}
-					
-					$('.cafeName').click(function(event) {
-						event.preventDefault();
-						location.href = '../cafeinfo/cafeinfo.html?cafeMemberNo=' + $(this).attr("data-no");
-					});
-					
-					
-					/* 결과창 높이 조절 */
-					$('.listArea').css('height', ($(window).height() - 110) + 'px');
-					
-					
-					/* 무한 스크롤 */
-					$(window).scroll(function() {
-						if($(window).scrollTop() + $(window).height() == $(document).height()) {loadPage(++pageCount);}
-					});
-				});
+		serverRoot + '/customCard/findCafe.json',
+		{customMemberNo: customMemberNo,
+		searchKeyword: searchKeyword,
+		postNo: postNo,
+		pageCount: pageCount},
+			
+		function(ajaxResult) {
+			var status = ajaxResult.status;
+			if (status != "success") {console.log(ajaxResult.data); return;}
+			var cafeList = ajaxResult.data.cafeList;
+			console.log(cafeList);
+			
+			if (cafeList == null) {
+				$('.cafeListArea #num').text(allCafeCount);
+			} else {
+				allCafeCount = ajaxResult.data.allCafeCount;
+				$('.cafeListArea #num').text(allCafeCount);
+			}
+			
+			var listArea = $('.listArea');
+			var template = Handlebars.compile($('#trTemplate').html());
+			
+			if (pageCount == 1 && searchKeyword != "") {
+				listArea.html(template({"cafeList": cafeList}));
+			} else {
+				listArea.append(template({"cafeList": cafeList}));
+			}
+			
+			$('.cafeName').click(function(event) {
+				event.preventDefault();
+				location.href = '../cafeinfo/cafeinfo.html?cafeMemberNo=' + $(this).attr("data-no");
+			});
+			
+			
+			/* 결과창 높이 조절 */
+			$('.listArea').css('height', ($(window).height() - 110) + 'px');
+			
+			
+			/* 무한 스크롤 */
+			$(window).scroll(function() {
+				if($(window).scrollTop() + $(window).height() == $(document).height()) {loadPage(++pageCount);}
+			}); // 스크롤 이벤트가 반복되어 일어나고 있음. 수정 필요.
+		}
+	);
 };
 
 
