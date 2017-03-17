2fff2c2a57d1b5116d78d523c8935e3dcccb4c05
sinaeYu <anne.sinae@gmail.com>
2017-03-17 11:26:15 +0900

    ,

--- src/main/java/bitcamp/java89/ems2/dao/MessageDao.java
+++ src/main/java/bitcamp/java89/ems2/dao/MessageDao.java
@@ -1,7 +1,10 @@
 package bitcamp.java89.ems2.dao;
 
+import java.util.ArrayList;
+
 import bitcamp.java89.ems2.domain.Message;
 
 public interface MessageDao {
 	int insertCustomMsg(Message message) throws Exception;
+	ArrayList<Message> getMsgList(Message message) throws Exception;
 }
