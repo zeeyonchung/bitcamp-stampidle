ceba4d9947d06f913d6f84689046f8e18c86d292
sinaeYu <anne.sinae@gmail.com>
2017-03-17 17:53:09 +0900

    message working

--- src/main/java/bitcamp/java89/ems2/control/json/MessageJsonControl.java
+++ src/main/java/bitcamp/java89/ems2/control/json/MessageJsonControl.java
@@ -17,24 +17,26 @@ public class MessageJsonControl {
   
   @Autowired MessageService messageService;
   
-  @RequestMapping(value = "/cstmr_m/message/addCustom")
-  public AjaxResult addCustom(Message message) throws Exception {
-  	messageService.addCustom(message);
+  @RequestMapping(value = "/cstmr_m/message/insertMsg")
+  public AjaxResult getMsgList(Message message) throws Exception {
+  	System.out.println("-------" + message.getContents());
+  	messageService.insertMsg(message);
     return new AjaxResult(AjaxResult.SUCCESS, "메시지 추가 성공");
   }
   
-  /*@RequestMapping(value = "/cstmr_m/message/addCafe")
-  public AjaxResult addCafe(Message message) throws Exception {
-  	messageService.addCustom(message);
-    return new AjaxResult(AjaxResult.SUCCESS, "메시지 추가 성공");
-  }
-  */
-  
-  @RequestMapping(value = "/cstmr_m/message/getMsgList")
+  @RequestMapping(value = "/cstmr_m/messageDetail/getMsgList")
   public AjaxResult getMsgList(int customMemberNo, int cafeMemberNo) throws Exception {
   	List<Message> list = messageService.getMsgList(customMemberNo, cafeMemberNo);
   	return new AjaxResult(AjaxResult.SUCCESS, list);
   }
+  
+  /*@RequestMapping(value = "/cstmr_m/message/getLastMsgList")
+  public AjaxResult getLastMsgList(int customMemberNo, int cafeMemberNo, String sendMember) throws Exception {
+  	List<Message> list = messageService.getLastMsgList(customMemberNo, cafeMemberNo, sendMember);
+  	return new AjaxResult(AjaxResult.SUCCESS, list);
+  }*/
+  
+  
 }
 
 
