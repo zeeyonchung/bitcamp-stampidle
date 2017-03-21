95486003884823604320a89cf9a9329642a27c98
GraysonSunkyoJung <graysons.jung@gmail.com>
2017-03-21 10:52:27 +0900

    admin 메세지 alert

--- src/main/webapp/admin/js/message.js
+++ src/main/webapp/admin/js/message.js
@@ -126,10 +126,12 @@ $.getJSON(serverRoot + '/auth/loginUser.json', function(ajaxResult) {
     
 	$('.btn-send').click(function() {
 		if ($('.chat-input').val() == "") {
-			alert("메시지를 입력해주세요.");
+			swal({title:"메시지를 입력해주세요.",
+				  type:"warning"});
 			return $('.chat-input').focus();
 		} else if ($('select[name=cafeNo]').val() == null){
-			alert("쪽지를 보내실 카페를 선택해주세요.")
+			swal({title:"쪽지를 보내실 카페를 선택해주세요.",
+				  type:"warning"});
 			return $('select[name=cafeNo]').focus();
 		}
     	var param = {
@@ -144,7 +146,9 @@ $.getJSON(serverRoot + '/auth/loginUser.json', function(ajaxResult) {
 	          alert(ajaxResult.data);
 	          return;
 	        }
-	        alert("쪽지를 보냈습니다.");
+	        swal({title: "쪽지를 보냈습니다.",
+	        	  type: "success"});
+	        
 	        $('.chat-input').val("");
 	        $('.writeWrap').fadeOut(200);
 	        getMsgSent();
