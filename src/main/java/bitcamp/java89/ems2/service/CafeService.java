3dfaa0634e80c1ddb5c5ea8b1ab8f1a40cf7a277
Zeeyon Chung <zeeyon.c@gmail.com>
2017-03-22 18:18:56 +0900

    .

--- src/main/java/bitcamp/java89/ems2/service/CafeService.java
+++ src/main/java/bitcamp/java89/ems2/service/CafeService.java
@@ -1,5 +1,7 @@
 package bitcamp.java89.ems2.service;
 
+import java.util.List;
+
 import bitcamp.java89.ems2.domain.Cafe;
 import bitcamp.java89.ems2.domain.CafeAdd;
 import bitcamp.java89.ems2.domain.CafeMember;
@@ -8,6 +10,7 @@ public interface CafeService {
   void add(CafeAdd cafeAdd) throws Exception;
   CafeMember getDetail(int cafeMemberNo) throws Exception;
   int update(Cafe cafe) throws Exception;
+  List<Cafe> getCafeMapList() throws Exception;
 }
 
 
