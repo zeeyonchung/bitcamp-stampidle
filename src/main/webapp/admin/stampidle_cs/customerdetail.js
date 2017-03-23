41a1692c82f2809fd621c37c7a99a067ece9a352
sinaeYu <anne.sinae@gmail.com>
2017-03-23 18:04:00 +0900

    css working for customdetail

--- src/main/webapp/admin/stampidle_cs/customerdetail.js
+++ src/main/webapp/admin/stampidle_cs/customerdetail.js
@@ -48,7 +48,7 @@ function loadPage(){
 	
 		
 		/**** 카드 뒷면 이미지 로드된 후 스탬프 영역, 찍힌 스탬프 가져오기 ****/
-		$('#card-back').load(function() {
+		$('#card-back').load(function(event) {
 			event.stopImmediatePropagation()
 			var width = $('#card-back').width();
 			var height = $('#card-back').height();
@@ -190,7 +190,7 @@ function loadPage(){
 		
 		$('#send-text').attr('data-no', customMemberNo);
 		$('#name').text(customCard.customName);
-		$('#email').text(customCard.email);
+		$('#email').text(customCard.customEmail);
 		$('#phone-number').text(customCard.customTel);
 		$('.finish-coupon').text(customCard.finishCardCount);
 		$('.first-visit-date').text(customCard.firstVisitDate);
