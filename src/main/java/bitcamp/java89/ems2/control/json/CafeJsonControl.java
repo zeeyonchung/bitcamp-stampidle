2cf261d4feb1d94ec4f902795a27b1332d371687
Zeeyon Chung <zeeyon.c@gmail.com>
2017-03-23 15:10:40 +0900

    .

--- src/main/java/bitcamp/java89/ems2/control/json/CafeJsonControl.java
+++ src/main/java/bitcamp/java89/ems2/control/json/CafeJsonControl.java
@@ -1,7 +1,5 @@
 package bitcamp.java89.ems2.control.json;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.ServletContext;
@@ -101,7 +99,7 @@ public class CafeJsonControl {
   
   
   @RequestMapping(value = {"/admin/comment/detail", "/cstmr_m/comment/detail"})
-  public AjaxResult getList(int cafeMemberNo, int customMeberNo) throws Exception {
+  public AjaxResult getList(int cafeMemberNo, int customMemberNo) throws Exception {
     List<Comment> list = commentService.getList(cafeMemberNo);
     return new AjaxResult(AjaxResult.SUCCESS, list);
   }
