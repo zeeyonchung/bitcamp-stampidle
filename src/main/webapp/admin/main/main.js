036be28d25cd854ee87e265f01c1e37e4e62c236
sinaeYu <anne.sinae@gmail.com>
2017-03-26 13:41:06 +0900

    .

--- src/main/webapp/admin/main/main.js
+++ src/main/webapp/admin/main/main.js
@@ -19,12 +19,12 @@ $.getJSON(serverRoot + '/auth/loginUser.json', function(ajaxResult) {
 				var allStampList = ajaxResult.data;
 				console.log(ajaxResult.data);
 				
-				$('#visit h2').text(allStampList.visitMember +"명");
-				$('#new-coupone h2').text(allStampList.finishCard + "개");
-				$('#give-stamp h2').text(allStampList.stampMany+ "개");
-				$('#new-member h2').text(allStampList.newMember+ "명");
-				$('#new-message h2').text(allStampList.messageCount + "개");
-				$('#free-drink h2').text(allStampList.freeItem+ "개");
+				$('#visit h2 .result-val').text(allStampList.visitMember);
+				$('#new-coupone h2 .result-val').text(allStampList.finishCard);
+				$('#give-stamp h2 .result-val').text(allStampList.stampMany);
+				$('#new-member h2 .result-val').text(allStampList.newMember);
+				$('#new-message h2 .result-val').text(allStampList.messageCount);
+				$('#free-drink h2 .result-val').text(allStampList.freeItem);
 				$('.cafeImgSlide img').attr('src', '../../upload/' + event.eventPhotoPath);
 				$('.event-cont-div .span-contents').text(event.eventContents);
 			});
