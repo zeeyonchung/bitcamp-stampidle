4b5bc7fdc19e9c12f83da4c8348cabe0acccb1bd
zeeyon chung <zeeyon.c@gmail.com>
2017-04-07 13:03:05 +0900

    정리

--- src/main/webapp/cstmr_m/js/context.js
+++ src/main/webapp/cstmr_m/js/context.js
@@ -1,2 +1,2 @@
-var serverRoot = 'http://b.bitcamp.com:8080/bitcamp_stampidle/cstmr_m';
-var clientRoot = '/bitcamp_stampidle/cstmr_m';
+var serverRoot = 'http://b.bitcamp.com:8080/bitcamp-stampidle/cstmr_m';
+var clientRoot = '/bitcamp-stampidle/cstmr_m';
